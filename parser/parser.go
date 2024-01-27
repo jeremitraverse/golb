10b200a8184799a564bc52aae2b77ed413d49d7d
jeremitraverse <jeremi.traverse@gmail.com>
2024-01-27 15:47:42 -0500

    parser impl

--- parser/parser.go
+++ parser/parser.go
@@ -1,7 +1,7 @@
 package parser
 
 import (
-	"fmt"
+	"strings"
 
 	"github.com/jeremitraverse/golb/line"
 	"github.com/jeremitraverse/golb/token"
@@ -12,9 +12,76 @@ type Parser struct {
 	currentToken token.Token
 }
 
-
-func (p *Parser) ParseLine(li line.Line) {
-	for tok := range li.Tokens {
-		fmt.Println(tok)
-	}
+func New(li line.Line) Parser {
+	return Parser{ currentLine: li }
 }
+
+func (p *Parser) ParseLine() string {
+	lineType := p.currentLine.Type
+
+	switch lineType {
+		case line.FIRST_TITLE:
+			lit := p.parseTextTokens()
+			return createHtmlNode("h1", lit)
+		case line.SECOND_TITLE:
+			lit := p.parseTextTokens()
+			return createHtmlNode("h2", lit)
+		case line.THIRD_TITLE:
+			lit := p.parseTextTokens()
+			return createHtmlNode("h3", lit)
+		case line.FOURTH_TITLE:
+			lit := p.parseTextTokens()
+			return createHtmlNode("h4", lit)
+		case line.TEXT:
+			lit := p.parseTextTokens()
+			return createHtmlNode("div", lit)
+		case line.IMAGE:
+			return p.parseImageLine()	
+	}
+
+	return ""
+}
+
+func (p *Parser) parseTextTokens() string {
+	var sb strings.Builder
+	
+	for _, tok := range p.currentLine.Tokens {
+		switch tok.Type {
+			case token.TEXT:
+				sb.WriteString(tok.Literal)
+			case token.BOLD:
+				node := createHtmlNode("b", tok.Literal) 
+				sb.WriteString(node)
+			case token.ITALIC:
+				node := createHtmlNode("i", tok.Literal) 
+				sb.WriteString(node)
+		}
+	}
+
+	return sb.String()
+}
+
+func (p *Parser) parseImageLine() string {
+	return "<img src=\"" + p.currentLine.Tokens[0].Literal + "\" />"
+}
+
+func getOpeningNode(nodeType string) string {
+	return "<" + nodeType + ">"
+}
+
+func getClosingNode(nodeType string) string {
+	return "</" + nodeType + ">"
+}
+
+func createHtmlNode(nodeType string, nodeLiteral string) string {
+	var sb strings.Builder
+
+	openingNode := getOpeningNode(nodeType)
+	closingNode := getClosingNode(nodeType)
+	node := openingNode + nodeLiteral + closingNode
+
+	sb.WriteString(node)
+
+	return sb.String()
+}
+
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,3 +1,83 @@
 package parser
 
+import (
+	"testing"
 
+	"github.com/jeremitraverse/golb/lexer"
+)
+
+func TestTitleParse(t* testing.T) {
+	input := `# _First Title_
+## **Second Title**
+### __Third Title__
+#### *Fourth Title*
+# _First Title_ Mix
+`
+
+	expectedStrings := []string { 
+		"<h1><i>First Title</i></h1>",
+		"<h2><b>Second Title</b></h2>",
+		"<h3><b>Third Title</b></h3>",
+		"<h4><i>Fourth Title</i></h4>",
+		"<h1><i>First Title</i> Mix</h1>",
+	}
+
+	lex := lexer.New(input)	
+	for i, expectedNode := range expectedStrings {
+		li := lex.GetLine()	
+		p := New(li)
+		
+		parsedLine := p.ParseLine()
+		if parsedLine != expectedNode {
+			t.Fatalf("Error at node #%d, expected %s got %s", i, expectedNode, parsedLine)
+		}
+	}	
+}
+
+func TestTextParsing(t* testing.T) {
+	input := `_Italic Text_
+*Italic Text* Regular Text
+**Bold Text** *Regular Text
+__Bold Text__ Regular Text*`
+
+	expectedStrings := []string { 
+		"<div><i>Italic Text</i></div>",
+		"<div><i>Italic Text</i> Regular Text</div>",
+		"<div><b>Bold Text</b> *Regular Text</div>",
+		"<div><b>Bold Text</b> Regular Text*</div>",
+	}
+
+	lex := lexer.New(input)	
+	for i, expectedNode := range expectedStrings {
+		li := lex.GetLine()	
+		p := New(li)
+			
+		parsedLine := p.ParseLine()
+
+		if parsedLine != expectedNode {
+			t.Fatalf("Error at node #%d, expected %s got %s", i, expectedNode, parsedLine)
+		}
+	}	
+}
+
+func TestImageParsing(t *testing.T) {
+	input := `<photo.png>
+< Text`
+	expectedStrings := []string { 
+		"<img src=\"photo.png\" />",
+		"<div>< Text</div>",
+	}
+
+	lex := lexer.New(input)	
+	for i, expectedNode := range expectedStrings {
+		li := lex.GetLine()	
+		p := New(li)
+			
+		parsedLine := p.ParseLine()
+
+		if parsedLine != expectedNode {
+			t.Fatalf("Error at node #%d, expected %s got %s", i, expectedNode, parsedLine)
+		}
+	}	
+
+}
